d0c5e78c4c1d59126f0702218030b6196b0bd5a7
Garrett Nickel <gmnicke2@illinois.edu>
2015-05-29 14:42:58 -0500

    Began shift over to one centralized rest call. All logger to stderr

--- cg_extras.py
+++ cg_extras.py
@@ -1,58 +1,47 @@
 import json
 import sys
 import requests
+from requests import exceptions as rex
 import logging
 import ipaddress
 import urlparse
 
-logger = logging.getLogger('CG LOGGER')
+def cg_rest(method, url, logger, **kwargs) :
+    method = method.upper()
+    try :
+        request_ret = requests.request(method,url,**kwargs)
+    except (rex.ConnectionError,
+        rex.HTTPError,
+        rex.MissingSchema) :
+        logger.error('Problem with API url - '
+                'Is it entered correctly?')
+        sys.exit(1)
+    except (rex.Timeout) :
+        logger.error('Request timed out.')
+        sys.exit(1)
+    return request_ret.json()
 
-# Since logger.error() will be followed by a 
-# logging.FileHandler.close() and an exit(),
-# this function helps there be less syntax with more indirection
-def reportError(message) :
-    # Print to stderr and log error message and timestamp
-    logger.error(message)
-    # Close file associated with the file handler
-    logger.handlers[1].close()
-    # Exit with error
-    sys.exit(1)
-
-# Initializes logger (defines what is written where and if log file clears)
-def logger_initialize(verbose,debug,clear) :
+# Initializes logger (defines what is written to stderr and if log file clears)
+def logger_initialize(name,verbose,debug) :
+    logger = logging.getLogger(name)
     logging_format = ("\n%(levelname)s - %(asctime)s\n%(message)s")
     formatter = logging.Formatter(logging_format)
-    # Errors, Warnings, and Criticals always printed to stderr
-    err_handler = logging.StreamHandler()
-    err_handler.setLevel(logging.WARNING)
-    err_handler.setFormatter(formatter)
-    # Create file handler to pipe necessary logger levels to a log file
-    # Clear the file if user specified --clearlog
-    # Otherwise, append
-    if clear :
-        file_handler = logging.FileHandler('cg_log.log', 'w')
-    else :
-        file_handler = logging.FileHandler('cg_log.log')
-    # If -d/--debug specified, log will contain ALL levels
-    # If -v/--verbose specifiged, log will only contain up to INFO level
-    # Otherwise, only ERROR, WARNING, CRITICAL in log
-    # If debug and verbose specified, debug chosen
+    stream_handler = logging.StreamHandler()
     if debug :
-        level = logging.DEBUG
-        file_handler.setLevel(level)
+        logger.setLevel(logging.DEBUG)
+        stream_handler.setLevel(logging.DEBUG)
     elif verbose :
-        level = logging.INFO
-        file_handler.setLevel(level)
+        logger.setLevel(logging.VERBOSE)
+        stream_handler.setLevel(logging.VERBOSE)
     else :
-        level = logging.WARNING
-        file_handler.setLevel(level)
-    file_handler.setFormatter(formatter)
-    logger.setLevel(level)
-    logger.addHandler(err_handler)
-    logger.addHandler(file_handler)
+        logger.setLevel(logging.WARNING)
+        stream_handler.setLevel(logging.WARNING)
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+    return logger
 
 # prints information if -v or --verbose specified
-def logResponse(request_type, request_json, response_json, url) :
+def logResponse(request_type, request_json, response_json, url,logger) :
     request_json['password'] = '*******'
     logger.debug("URL: " + url)
     logger.debug("Request: " + request_type)
@@ -62,23 +51,25 @@ def logResponse(request_type, request_json, response_json, url) :
         ": " + json.dumps(response_json,indent=4,separators=(',',': ')))
 
 # checks for errors in response and prints necessary info to stderr
-def check_for_response_errors(response_json) :
+def check_for_response_errors(response_json,logger) :
     logger.debug("Checking for errors from server's response")
     try :
         status = response_json['status']
     except KeyError :
-        reportError("\nResponse JSON failed to create.")
+        logger.error("\nResponse JSON failed to create.")
+        sys.exit(1)
     logger.debug("Response JSON created successfully")
     if(status != 'success') :
-        reportError('Request Failed\n'
+        logger.error('Request Failed\n'
             'Error %d: "%s"' 
             %(response_json['result']['error_code'],
             response_json['result']['message'])
             )
+        sys.exit(1)
     logger.debug('Response JSON indicates "success"')
 
 # makes sure URL passes some tests (mostly if dealing with url as an IP)
-def check_url_validity(url) :
+def check_url_validity(url,logger) :
     logger.debug('Testing URL host name validity')
     host_name = urlparse.urlparse(url).hostname
     # make sure host name isn't localhost
--- cg_token.py
+++ cg_token.py
@@ -12,8 +12,6 @@ import getpass
 # Used to disable InsecureRequestWarning that occurs with this API
 requests.packages.urllib3.disable_warnings()
 
-logger = logging.getLogger('CG LOGGER')
-
 # any argument used to overwrite environ vars is stored here;
 # it is accessed throughout the code with the format:
 # env_overwrite.get(<KEY>,<If KEY doesn't exist use environ or its default -- usually ''>)
@@ -60,7 +58,7 @@ def parseArgs() :
         if getattr(args,arg) :
             env_overwrite[arg] = getattr(args,arg)
     # Initialize Logger
-    logger_initialize(args.verbose,args.debug,args.clearlog)
+    logger = logger_initialize(__name__,args.verbose,args.debug)
     # Append a terminating '/' if non-existent in API url
     if (env_overwrite.get('url','') 
         and not env_overwrite.get('url','').endswith('/') 
@@ -71,13 +69,14 @@ def parseArgs() :
          ) :
         os.environ['CG_API_URL'] += '/'
     elif (not os.getenv('CG_API_URL','')) :
-        reportError('CG_API_URL (API url for REST calls)' 
+        logger.error('CG_API_URL (API url for REST calls)' 
                 'not specified')
-    return (parser,args,args.action.lower())
+        sys.exit(1)
+    return (parser,args,args.action.lower(),logger)
 
 ############################API CALLS##################################
 #issue token
-def issueToken(username,password,url) :
+def issueToken(username,password,url,logger) :
     # A sample issue token request JSON
     request_json = {'username' : username,
         'password' : password,
@@ -86,38 +85,32 @@ def issueToken(username,password,url) :
     }
     # Append resource ("token") to API url
     url += 'token'
-    check_url_validity(url)
+    check_url_validity(url,logger)
     # Make RESTful POST call to "token" resource
     # Revoke would use DELETE, Verify would use PUT
-    try :
-        request_ret = requests.post(url,
-            data=request_json,
-            timeout=50,
-            verify=False)
-    except (rex.ConnectionError, 
-        rex.HTTPError, 
-        rex.MissingSchema) :
-        reportError('Problem with API url - ' 
-                'Is it entered correctly?')
-    except (rex.Timeout) :
-        reportError('Request timed out.')
-    # Get the response from the REST POST in JSON format
-    response_json = request_ret.json()
-    check_for_response_errors(response_json)
+    response_json = cg_rest('POST',
+        url,
+        logger,
+        data=request_json,
+        timeout=50,
+        verify=False)
+    check_for_response_errors(response_json,logger)
     try :
         token = response_json['result']['token']
     except (TypeError,KeyError) :
-        reportError("Token creation failed. "
+        logger.error("Token creation failed. "
                 "(Check your arguments)\n")
+        sys.exit(1)
     logResponse('Issue Token (HTTP POST)',
         request_json,
         response_json,
-        url)
+        url,
+        logger)
     logger.info("Token %s created successfully" %token)
     return token
 
 # verify token
-def verifyToken(username,password,url,client_id,client_ip,token) :
+def verifyToken(username,password,url,client_id,client_ip,token,logger) :
     request_json = {
         'consumer' : client_id,
         'remote_addr' : client_ip,
@@ -125,63 +118,50 @@ def verifyToken(username,password,url,client_id,client_ip,token) :
         'username' : username
     }
     url += 'token'
-    check_url_validity(url)
+    check_url_validity(url,logger)
     request_length = str(len(json.dumps(request_json)))
     # Set HTTP Header
     headers = {'Content-Length' : request_length}
-    try :
-        # Make RESTful PUT call
-        request_ret = requests.put(url,
-            data=request_json,
-            headers=headers,
-            timeout=50,
-            verify=False)
-    except (rex.ConnectionError,
-        rex.HTTPError,
-        rex.MissingSchema) :
-        reportError('Problem with API url - '
-                'Is it entered correctly?')
-    except (rex.Timeout) :
-        reportError('Request timed out.')
-    response_json = request_ret.json()
-    check_for_response_errors(response_json)
+    # Make RESTful PUT call
+    response_json = cg_rest('PUT',
+        url,
+        logger,
+        data=request_json,
+        headers=headers,
+        timeout=50,
+        verify=False)
+    check_for_response_errors(response_json,logger)
     logResponse('Verify Token "%s" (HTTP PUT)' %(token),
         request_json,
         response_json,
-        url)
+        url,
+        logger)
     if response_json['status'] == 'success' :
         return True
     else :
         return False
 
 # revoke token
-def revokeToken(username,password,url,token) :
+def revokeToken(username,password,url,token,logger) :
     request_json = {
         'username' : username,
-            'password' : password,
+        'password' : password,
         'token' : token
     }
     url += 'token'
-    check_url_validity(url)
-    try :
-        # Make RESTful DELETE call
-        request_ret = requests.delete(url,
-            params=request_json,
-            timeout=50,
-            verify=False)
-    except (rex.ConnectionError,
-        rex.HTTPError,
-        rex.MissingSchema) :
-        reportError('Problem with API url - '
-                'Is it entered correctly?')
-    except (rex.Timeout) :
-        reportError('Request timed out.')
-    response_json = request_ret.json()
-    check_for_response_errors(response_json)
+    check_url_validity(url,logger)
+    response_json = cg_rest('DELETE',
+        url,
+        logger,
+        params=request_json,
+        timeout=50,
+        verify=False)
+    check_for_response_errors(response_json,logger)
     logResponse('Revoke Token %s (HTTP DELETE)' %token,
         request_json,
         response_json,
-        url)
+        url,
+        logger)
     # Token was revoked successfully, so store empty string as environ token
     if response_json['status'] == 'success' :
         logger.info('Token %s successfully revoked' %token)
@@ -191,7 +171,7 @@ def revokeToken(username,password,url,token) :
         return False
 
 def main() :
-    (parser,args,action) = parseArgs()
+    (parser,args,action,logger) = parseArgs()
     # Retrieve necessary info (either from env or overwritten while parsing)
     username = env_overwrite.get('username', 
         os.getenv('CG_USERNAME',''))
@@ -206,12 +186,14 @@ def main() :
         logger.info("ISSUING TOKEN")
         print issueToken(username,
             password,
-            url)
+            url,
+            logger)
     else :
         token = env_overwrite.get('token',
             os.getenv('CG_TOKEN',''))
         if not token :
-            reportError('No valid CG_TOKEN given')
+            logger.error('No valid CG_TOKEN given')
+            sys.exit(1)
         if action == "verify" :
             client_id = env_overwrite.get('clientid', 
                 os.getenv('CG_CLIENT_ID',''))
@@ -223,17 +205,17 @@ def main() :
                 url,
                 client_id,
                 client_ip,
-                token)
+                token,
+                logger)
         elif action == "revoke" :
             logger.info('REVOKING TOKEN "%s"' %token)
             revokeToken(username,
                 password,
                 url,
-                token)
+                token,
+                logger)
         else :
             parser.print_help()
             exit()
-    logger.handlers[1].flush()
-    logger.handlers[1].close()
 
 main()
